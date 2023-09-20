0c6a22f6a6856f586c94057445d66237d98ad759
marco.bonelli <marco.bonelli@bigsrl.it>
2023-09-20 10:48:42 +0200

    20/09 Add MqttNet e verifica della connessione al broker

---
+++ ClientMqtt_Test/ClientMqtt_Test/ClientManager.cs
@@ -0,0 +1,47 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+
+public class ClientManager
+{
+    /// <summary>
+    /// Connect the Client to the Broker
+    /// </summary>
+    /// <param name="Server">Address of the broker</param>
+    /// <param name="MqttClient">Client istante to connect</param>
+    /// <returns></returns>
+    public async Task Connect(string Server, IMqttClient MqttClient)
+    {
+        try
+        {
+            var mqttOpzioni = new MqttClientOptionsBuilder()
+                .WithTcpServer(Server)
+                .WithProtocolVersion(MQTTnet.Formatter.MqttProtocolVersion.V311)
+                .Build();
+
+            var response = await MqttClient.ConnectAsync(mqttOpzioni, CancellationToken.None);
+
+            if (response.ResultCode == MqttClientConnectResultCode.Success)
+            {
+                Console.WriteLine("Test di connessione completato con successo!");
+            }
+            else
+                Console.WriteLine("Attenzione: impossibile connettersi al Brocker!");
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Attenzione: impossibile connettersi al Brocker!");
+        }
+    }
+
+    /// <summary>
+    /// Disconnect the Client from the Brocker
+    /// </summary>
+    /// <param name="MqttFactory"></param>
+    /// <param name="MqttClient"></param>
+    /// <returns></returns>
+    public async Task Disconnect(MqttFactory MqttFactory, IMqttClient MqttClient)
+    {
+        var mqttOpzioni = MqttFactory.CreateClientDisconnectOptionsBuilder().Build();
+        await MqttClient.DisconnectAsync(mqttOpzioni, CancellationToken.None);
+    }
+}
--- ClientMqtt_Test/ClientMqtt_Test/Program.cs
+++ ClientMqtt_Test/ClientMqtt_Test/Program.cs
@@ -1,11 +1,20 @@
-﻿
-
-bool _keepAlive = true;
-string _command = "";
+﻿using MQTTnet;
+using MQTTnet.Client;
 
 ///Tengo la console sveglia finchè non sfrutto il comando per uscire
 ///non sono limitato ad un numero di comandi
 ///esco quando la condizione nel while non è più vera
+bool _keepAlive = true;
+string _command = "";
+
+///Indirizzi di Broker Mqtt per test
+///il 127.0.0.1 fa riferimento a quello di default di Eclipse-Mosquitto su Docker
+string _server = "127.0.0.1";
+//string _server = "test.mosquitto.org";
+//string _server = "broker.hivemq.com";
+
+
+ClientManager _managerMqtt = new ClientManager();
 
 //Primo comando descrittivo
 Console.Write($"Ciao,\n " +
@@ -28,6 +37,7 @@ do
             break;
         case ("help"):
             Console.WriteLine("timer: esegue un test dello Sleep.");
+            Console.WriteLine("verifyconnection: esegue un test di connessione del client al broker.");
             break;
         case ("timer"):
             Thread.Sleep(1000);
@@ -39,6 +49,14 @@ do
             Thread.Sleep(5000);
             Console.WriteLine("10 secondi");
             Console.WriteLine("FINE");
+            break;
+        case ("verifyconnection"):
+            MqttFactory mqttFactory = new MqttFactory();
+            using (var mqttClient = mqttFactory.CreateMqttClient())
+            {
+                await _managerMqtt.Connect(_server, mqttClient);
+                await _managerMqtt.Disconnect(mqttFactory, mqttClient);
+            }
             break;
     }
 } while (_keepAlive);
