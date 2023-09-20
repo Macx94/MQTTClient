c31370932fd9bf14c6b5d77a123716325a3bcad8
marco.bonelli <marco.bonelli@bigsrl.it>
2023-09-20 10:26:44 +0200

    20/09 Base comportamento Console App

--- ClientMqtt_Test/ClientMqtt_Test/Program.cs
+++ ClientMqtt_Test/ClientMqtt_Test/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+bool _keepAlive = true;
+string _command = "";
+
+///Tengo la console sveglia finchè non sfrutto il comando per uscire
+///non sono limitato ad un numero di comandi
+///esco quando la condizione nel while non è più vera
+do
+{
+    //Primo comando descrittivo
+    Console.Write($"Ciao,\n" +
+        $"Digita un comando oppure scrivi 'exit' per uscire.\n");
+
+    //Leggo l'input digitato nella console
+    _command = Console.ReadLine();
+
+    switch (_command.ToLower())
+    {
+        default:
+            Console.Write("Comando non riconosciuto.\ninserisci un comando.\n");
+            break;
+        case ("exit"):
+            //Esco senza stampare comandi
+            _keepAlive = false;
+            break;
+    }
+} while (_keepAlive);
