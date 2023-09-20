f875287e729e1766ba8567e480af82f1827844a2
marco.bonelli <marco.bonelli@bigsrl.it>
2023-09-20 10:40:49 +0200

    20/09 Aggiunta Comando Help e Test del Timer

--- ClientMqtt_Test/ClientMqtt_Test/Program.cs
+++ ClientMqtt_Test/ClientMqtt_Test/Program.cs
@@ -6,11 +6,13 @@ string _command = "";
 ///Tengo la console sveglia finchè non sfrutto il comando per uscire
 ///non sono limitato ad un numero di comandi
 ///esco quando la condizione nel while non è più vera
+
+//Primo comando descrittivo
+Console.Write($"Ciao,\n " +
+    $"scrivi 'help' per avere un suggerimento.\n " +
+    $"Scrivi 'exit' per uscire.\n");
 do
 {
-    //Primo comando descrittivo
-    Console.Write($"Ciao,\n" +
-        $"Digita un comando oppure scrivi 'exit' per uscire.\n");
 
     //Leggo l'input digitato nella console
     _command = Console.ReadLine();
@@ -23,6 +25,20 @@ do
         case ("exit"):
             //Esco senza stampare comandi
             _keepAlive = false;
+            break;
+        case ("help"):
+            Console.WriteLine("timer: esegue un test dello Sleep.");
+            break;
+        case ("timer"):
+            Thread.Sleep(1000);
+            Console.WriteLine("1 secondo");
+            Thread.Sleep(1000);
+            Console.WriteLine("2 secondi");
+            Thread.Sleep(3000);
+            Console.WriteLine("5 secondi");
+            Thread.Sleep(5000);
+            Console.WriteLine("10 secondi");
+            Console.WriteLine("FINE");
             break;
     }
 } while (_keepAlive);
